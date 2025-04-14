82f81c33b7b1bd0269e1d0776c695fce185e4f3f
mehekdaga14 <daga.m@northeastern.edu>
2025-04-13 23:38:03 -0400

    Update boxplot.js

--- 4200PROJECT/boxplot.js
+++ 4200PROJECT/boxplot.js
@@ -1,13 +1,11 @@
 //
 
 const margin = { top: 50, right: 30, bottom: 60, left: 100 },
-      width = 960 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
+      width = 960,
+      height = 600;
 
-const svg = d3.select("#chart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+const svg = d3.select("svg")
+  .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
 const tooltip = d3.select(".tooltip");
